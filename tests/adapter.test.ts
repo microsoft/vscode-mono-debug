d8bb96f3eb5070457ffd8c2262b5c841c9bdd5b1
Andre Weinand <aweinand@microsoft.com>
2016-02-21 01:54:36 +0100

    fix test

--- tests/adapter.test.ts
+++ tests/adapter.test.ts
@@ -26,7 +26,7 @@ suite('Node Debug Adapter', () => {
 	});
 
 	teardown(done => {
-		dc.stop(done); 
+		dc.stop(done);
 	});
 
 	suite('basic', () => {
@@ -44,7 +44,7 @@ suite('Node Debug Adapter', () => {
 
 		test('should produce error for invalid \'pathFormat\'', done => {
 			dc.initializeRequest({
-				adapterID: 'mock',
+				adapterID: 'mono',
 				linesStartAt1: true,
 				columnsStartAt1: true,
 				pathFormat: 'url'
