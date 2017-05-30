d1c1aa05cbdda1f44c5f5f0c0baa723d998d6a7a
Vadzim Vysotski <vadzimv@playtika.com>
2017-05-30 21:41:40 +0300

    fixed set prop

--- src/MonoDebugSession.cs
+++ src/MonoDebugSession.cs
@@ -441,7 +441,7 @@ namespace VSCodeDebug
 
 			var forwardOutput = RunAdb("forward tcp:0 tcp:10000");
 			var port = int.Parse(forwardOutput);
-			RunAdb("shell setprop port=10000,timeout=2000000000");
+			RunAdb("shell setprop debug.mono.connect port=10000,timeout=2000000000");
 			RunAdb($"shell am force-stop {packageName}");
 			RunAdb($"shell monkey -p {packageName} -c android.intent.category.LAUNCHER 1");
 
